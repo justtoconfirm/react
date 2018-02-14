83f2b4cf420e32d8cb4ba629041fa62f0a2a4aba
Justtoconfirm <office@justtoconfirm.co.uk>
2018-02-14 20:55:08 +0000

    Restructuring of the file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: './app/index.js',
-	module: {
-		rules: [
-			{
-				test: /\.(s*)css$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
-		]
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	}
+    entry: ['./app/js/index.js', './app/scss/main.scss'],
+    module: {
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader'],
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({filename: 'styles/style.css'}),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'scripts/main.js'
+    }
 };
