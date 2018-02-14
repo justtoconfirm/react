b824cd53d4656360e0d6dba99145e3883ebc07f7
Justtoconfirm <office@justtoconfirm.co.uk>
2018-02-14 20:26:40 +0000

    webpack.config.js file updated with loaders to compile scss

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const path = require('path');
 
 module.exports = {
 	entry: './app/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.(s*)css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}
+		]
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
