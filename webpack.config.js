0b1b431c73f0756e8c0816e05953858850882c64
Justtoconfirm <office@justtoconfirm.co.uk>
2018-03-15 20:08:50 +0000

    Revision to the webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,10 @@ module.exports = {
                             options: {
                                 minimize: true
                             } 
-                        }, 
-                        'sass-loader'
+                        },
+                        {
+                            loader: 'sass-loader'
+                        } 
                     ]
                 })
             }
