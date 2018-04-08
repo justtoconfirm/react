c0a3c0398031b3e17d7b7b073abfe24cc35c6596
Justtoconfirm <office@justtoconfirm.co.uk>
2018-04-08 21:27:33 +0100

    Comments removed

--- src/components/1-atoms/input/Input.js
+++ src/components/1-atoms/input/Input.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 function Input ({ name, type = 'text', placeholder = '' }) {
 	return (
-		//<input type={type} placeholder={placeholder} className='input' />
 		<input {...{ type, placeholder }} className='input' />
 	)
 }
