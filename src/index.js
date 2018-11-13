331dd8c9db239818575185f3c5fbe2d2b1b6d96f
Dan Jackson <dan.jackson@crunch.co.uk>
2018-11-13 08:10:57 +0000

    Redux files moved into 'redux' directory within 'src'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './store/index'
-import { addArticle } from './actions/index'
+import store from './redux/store/index'
+import { addArticle } from './redux/actions/index'
 import App from './components/App'
 
 /*
